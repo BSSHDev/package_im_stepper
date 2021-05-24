ad567508890aebc68d0a62ffdd77d2ee9d3e4e62
hebe_xu <53813919+hebe-xu@users.noreply.github.com>
2021-05-24 15:54:13 +0800

    Update base_stepper.dart

--- lib/src/first_stepper/core/base_stepper.dart
+++ lib/src/first_stepper/core/base_stepper.dart
@@ -82,7 +82,7 @@ class BaseStepper extends StatefulWidget {
   /// Creates a basic stepper.
   BaseStepper({
     this.children,
-    this.titles,
+    this.titles=[""],
     this.nextPreviousButtonsDisabled = true,
     this.stepTappingDisabled = true,
     this.previousButtonIcon,
@@ -273,7 +273,7 @@ class _BaseStepperState extends State<BaseStepper> {
             spacing: 5.0,
             axis: axis,
           ),
-        widget.titles.isEmpty?Text(""):Text(widget.titles[index]),
+        widget.titles.length==1?Text(widget.titles[0]):Text(widget.titles[index]),
         DottedLine(
             length: widget.lineLength/2,
             color: widget.lineColor ?? Colors.blue,
