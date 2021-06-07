5acb64bd746a18d20a8b6dc14aedfb440a19cefc
hebe_xu <53813919+hebe-xu@users.noreply.github.com>
2021-06-07 14:23:04 +0800

    Update base_stepper.dart

--- lib/src/first_stepper/core/base_stepper.dart
+++ lib/src/first_stepper/core/base_stepper.dart
@@ -343,7 +343,7 @@ class _BaseStepperState extends State<BaseStepper> {
   /// The previous button.
   Widget _previousButton() {
     return IgnorePointer(
-      ignoring: min == 0,
+      ignoring: prenext == 0,
       child: IconButton(
         visualDensity: VisualDensity.compact,
         icon: widget.previousButtonIcon ??
@@ -359,7 +359,7 @@ class _BaseStepperState extends State<BaseStepper> {
   /// The next button.
   Widget _nextButton() {
     return IgnorePointer(
-      ignoring: max == widget.children!.length - 1,
+      ignoring: prenext == widget.children!.length - 1,
       child: IconButton(
         visualDensity: VisualDensity.compact,
         icon: widget.nextButtonIcon ??
@@ -379,39 +379,9 @@ class _BaseStepperState extends State<BaseStepper> {
   index: prenext,
   duration: widget.stepReachedAnimationDuration,
         curve: widget.stepReachedAnimationEffect);
-   print(prenext);
-    ValueListenableBuilder<Iterable<ItemPosition>>(
-        valueListenable: itemPositionsListener.itemPositions,
-        builder: (context, positions, child) {
-         
-          if (positions.isNotEmpty) {
-            // Determine the first visible item by finding the item with the
-            // smallest trailing edge that is greater than 0.  i.e. the first
-            // item whose trailing edge in visible in the viewport.
-            min = positions
-                .where((ItemPosition position) => position.itemTrailingEdge > 0)
-                .reduce((ItemPosition min, ItemPosition position) =>
-                    position.itemTrailingEdge < min.itemTrailingEdge
-                        ? position
-                        : min)
-                .index;
-            // Determine the last visible item by finding the item with the
-            // greatest leading edge that is less than 1.  i.e. the last
-            // item whose leading edge in visible in the viewport.
-            max = positions
-                .where((ItemPosition position) => position.itemLeadingEdge < 1)
-                .reduce((ItemPosition max, ItemPosition position) =>
-                    position.itemLeadingEdge > max.itemLeadingEdge
-                        ? position
-                        : max)
-                .index;
-          }
-         return Card();
-        },
-      );
+  
    setState(() {
-       print(min);
-     print(max);
+     print(prenext);
       });
  }
 
@@ -422,40 +392,11 @@ class _BaseStepperState extends State<BaseStepper> {
   index: prenext,
   duration: widget.stepReachedAnimationDuration,
         curve: widget.stepReachedAnimationEffect);
-    print(prenext);
-     ValueListenableBuilder<Iterable<ItemPosition>>(
-        valueListenable: itemPositionsListener.itemPositions,
-        builder: (context, positions, child) {
-         
-          if (positions.isNotEmpty) {
-            // Determine the first visible item by finding the item with the
-            // smallest trailing edge that is greater than 0.  i.e. the first
-            // item whose trailing edge in visible in the viewport.
-            min = positions
-                .where((ItemPosition position) => position.itemTrailingEdge > 0)
-                .reduce((ItemPosition min, ItemPosition position) =>
-                    position.itemTrailingEdge < min.itemTrailingEdge
-                        ? position
-                        : min)
-                .index;
-            // Determine the last visible item by finding the item with the
-            // greatest leading edge that is less than 1.  i.e. the last
-            // item whose leading edge in visible in the viewport.
-            max = positions
-                .where((ItemPosition position) => position.itemLeadingEdge < 1)
-                .reduce((ItemPosition max, ItemPosition position) =>
-                    position.itemLeadingEdge > max.itemLeadingEdge
-                        ? position
-                        : max)
-                .index;
-          }
-         return Card();
-        },
-      );
+    
+    
 
     setState(() {
-        print(min);
-     print(max);
+     print(prenext);
       });
     
  }
